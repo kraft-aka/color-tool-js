0749cacb8741adf201c2825b4a1ca8cb6315d1ff
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-09-26 08:52:23 +0200

    uodate input and altered colors

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const hexInput = document.getElementById("hex-input");
 const sliderText = document.getElementById("slider-text");
 const slider = document.getElementById("slider");
 let inputColor = document.getElementById("input-color");
+const alteredColor = document.getElementById("altered-color");
+const alteredColorText = document.getElementById('altered-color-text');
 
 // assigns background color taken from input
 hexInput.addEventListener("keyup", () => {
@@ -14,7 +16,7 @@ hexInput.addEventListener("keyup", () => {
 // validates input value
 const isValid = (hex) => {
   if (!hex) return false;
-
+  
   const strippedStr = hex.replace("#", "");
   return strippedStr.length === 3 || strippedStr.length === 6;
 };
@@ -43,7 +45,6 @@ const hexToRgb = (hex) => {
 };
 
 // converts RGB to HEX
-
 const rgbToHex = (r, g, b) => {
   const h = ("0" + r.toString(16)).slice(-2);
   const e = ("0" + g.toString(16)).slice(-2);
@@ -52,12 +53,6 @@ const rgbToHex = (r, g, b) => {
   return `#${h}${e}${x}`;
 };
 
-// console.log(rgbToHex(255, 0, 238));
-
-// eventlistener for slider input
-slider.addEventListener("input", (e) => {
-  sliderText.innerHTML = `${e.target.value}%`;
-});
 
 // checks the num value if it in range from 0 to 255
 const increaseWithin0to255 = (hex, amount) => {
@@ -68,7 +63,6 @@ const increaseWithin0to255 = (hex, amount) => {
 
   return newHex;
 };
-
 
 // alter color with percentage
 const alterColor = (hex, percentage) => {
@@ -84,4 +78,13 @@ const alterColor = (hex, percentage) => {
 
 //console.log(rgbToHex(255, 255, 255));
 console.log(alterColor("584", 25));
+
+// eventlistener for slider input
+slider.addEventListener("input", () => {
+  if (!isValid(hexInput.value)) return;
+  sliderText.innerHTML = `${slider.value}%`;
+  const newColor = alterColor(hexInput.value, slider.value);
+  alteredColorText.innerText = `Altered Color: ${newColor}`
+  alteredColor.style.backgroundColor = newColor;
+});
 
